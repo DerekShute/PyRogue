e95ca760f44c1cb1f2a2ef0e2cf7f221ced4d55f
DerekShute <67981895+DerekShute@users.noreply.github.com>
2021-06-11 08:08:21 -0400

    Refactor position to accept both (x,y) and ((x,y)) (#14)
    * Refactor position to accept both (x,y) and ((x,y))
    
    because frequent typos

--- position.py
+++ position.py
@@ -2,7 +2,7 @@
     Position!
 """
 from __future__ import annotations
-from typing import Tuple
+from typing import Any, Tuple
 
 
 # ===== Pos ===============================================
@@ -16,17 +16,18 @@ class Pos:  # struct coord
     _x: int = 0
     _y: int = 0
 
-    def __init__(self, pos: Tuple = None, x: int = None, y: int = None):
-        """
-        Params
-            pos : tuple (x, y)
-        """
-        if pos is not None:
-            self._x, self._y = tuple(pos)  # "unpack" is the magic word
-        elif x is not None:
+    def __init__(self, x: Any = None, y: int = None):
+        self._x = 0
+        self._y = 0
+        if x is None and y is None:
+            self._x = 0
+            self._y = 0
+        elif type(x) is int:
             self._x = x
-        if y is not None:
-            self._y = y
+            if y is not None:
+                self._y = y
+        else:
+            self._x, self._y = tuple(x)  # "unpack"
 
     def __eq__(self, other) -> bool:
         return self._x == other._x and self._y == other._y
@@ -36,6 +37,9 @@ class Pos:  # struct coord
 
     def __str__(self) -> str:
         return f'@({self._x},{self._y})'
+
+    def __repr__(self) -> str:
+        return f'Pos({self._x},{self._y})'
 
     def __iter__(self):
         """
@@ -69,20 +73,6 @@ class Pos:  # struct coord
 
 # ===== TESTING ===========================================
 
-if __name__ == "__main__":
-    p = Pos((10, 20))
-    # print(p)
-    assert str(p) == '@(10,20)'
-    # print(Pos(p))
-    # print(p.x)
-    assert p.x == 10
-    # print(p.y)
-    assert p.y == 20
-    # print(p.xy)
-    assert p.xy == (10, 20)
-    assert not Pos((1, 1)) == Pos((2, 2))
-    assert Pos((1, 2)) == Pos(x=1, y=2)
-
-    print('*** Tests passed ***')
+# Is in test_position.py
 
 # EOF
---
+++ test_position.py
@@ -0,0 +1,37 @@
+"""
+    Unit test of position
+"""
+
+import unittest
+from position import Pos
+
+
+class TestPosition(unittest.TestCase):
+    """Test position"""
+
+    # Could actually be many tests, but why bother?
+    def test(self):
+        p = Pos((10, 20))
+        assert str(p) == '@(10,20)'
+        assert p.x == 10
+        assert p.y == 20
+        assert p.xy == (10, 20)
+
+        p = Pos(15, 25)
+        assert str(p) == '@(15,25)'
+        assert eval(repr(p)) == p
+
+        assert Pos(5, 15) == Pos(Pos(5, 15))
+
+        assert not Pos(1, 1) == Pos(2, 2)
+        assert Pos(1, 2) == Pos(x=1, y=2)
+
+        self.assertTrue(True)
+
+
+# ===== Invocation ========================================
+
+if __name__ == '__main__':
+    unittest.main()
+
+# EOF
