63b42a192f6bf9d592470fdcba92aca3c96321e6
DerekShute <67981895+DerekShute@users.noreply.github.com>
2021-06-24 16:58:41 -0400

    going for broke here

--- run_tests.py
+++ run_tests.py
@@ -2,13 +2,15 @@
     Unit test master script
     
     If you have 'tests' in the name, then pytest will try to do something with it.
+    
+    NOTE: IDENTICAL TO pipeline
 """
 
 import pytest
 import sys
 
-
-sys.path.insert(0, './pyrogue/')
-pytest.main(["tests", "--omit", "pipeline.py"])
+if __name__ == '__main__':
+    sys.path.insert(0, './pyrogue/')
+    pytest.main(["tests"])
 
 # EOF
