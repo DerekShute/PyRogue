aa7bb32575dbe64c7dde792e23c1b3769523237f
DerekShute <67981895+DerekShute@users.noreply.github.com>
2021-07-02 08:37:00 -0400

    RIP screen
    Death or cowardly quit

--- tests/test_gameloop.py
+++ tests/test_gameloop.py
@@ -4,8 +4,8 @@
 
 import unittest
 from unittest.mock import Mock
-from gameloop import MainMenuState, MainGameloop
-
+from gameloop import MainMenuState, MainGameloop, RIPGameState
+from player import Player
 
 # ===== Test Main Menu ====================================
 
@@ -33,6 +33,34 @@ class TestMainMenuState(unittest.TestCase):
         loop = prev_loop.run()
         assert loop == prev_loop
         self.assertTrue(True)
+
+class TestRIPState(unittest.TestCase):
+    """Test Main Menu"""
+
+    def test_quit(self):
+        p = Player.factory()
+        p.add_exp(1000)
+        p.purse = 500
+        p.levelno = 4
+        event_return = Mock(return_value='quit')
+        msg_mock = Mock()
+        display_mock = Mock(dispatch_event=event_return, centered_msg=msg_mock)
+        loop = RIPGameState(display=display_mock, player=p, situation='quit').run()
+        assert loop is None
+        assert msg_mock.call_count == 4
+        expected = ('A COWARDLY ESCAPE BY',
+                    'A level 7 adventurer',
+                    'On level 4 of The Dungeon of Doom',
+                    'Clutching 500 gold pieces')
+        i = 0
+        for string in expected:
+            _, kwargs = msg_mock.call_args_list[i] # call_args_list[x][0] is args, [1] is kwargs when unpacked
+            print(kwargs['string'])
+            assert kwargs['string'] == string
+            i += 1
+        self.assertTrue(True)
+
+    # TODO: death and victory
 
 
 # ===== Invocation ========================================
