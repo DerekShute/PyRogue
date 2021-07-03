7bd91898143504f68601df6cd933d6034f7443b2
DerekShute <67981895+DerekShute@users.noreply.github.com>
2021-07-03 17:34:25 -0400

    Weapons

--- pyrogue/procgen.py
+++ pyrogue/procgen.py
@@ -4,7 +4,7 @@
 import random
 from typing import List
 from item import Item, Food, Equipment
-from factories import calc_probability, ITEM_PROB_TEMPLATES, ARMOR_TEMPLATES
+from factories import calc_probability, ITEM_PROB_TEMPLATES, ARMOR_TEMPLATES, WEAPON_TEMPLATES
 
 
 # TODO: randmonster
@@ -15,6 +15,8 @@ ITEM_PROBABILITIES: List[int] = calc_probability(ITEM_PROB_TEMPLATES)
 ARMOR_PROBABILITIES: List[int] = calc_probability(ARMOR_TEMPLATES)
 """Initialized weighted probablility list"""
 
+WEAPON_PROBABILITIES: List[int] = calc_probability(WEAPON_TEMPLATES)
+"""Initialized weighted probability list"""
 
 # ===== Service Routines ==================================
 
@@ -33,6 +35,11 @@ def new_armor() -> Equipment:
     numlist = random.choices(list(range(0, len(ARMOR_TEMPLATES))), weights=ARMOR_PROBABILITIES, k=1)
     return Equipment.factory(etype=Equipment.ARMOR, template=ARMOR_TEMPLATES[numlist[0]])
 
+def new_weapon() -> Equipment:
+    """Fabricate a random weapon"""
+    numlist = random.choices(list(range(0, len(WEAPON_TEMPLATES))), weights=WEAPON_PROBABILITIES, k=1)
+    return Equipment.factory(etype=Equipment.WEAPON, template=WEAPON_TEMPLATES[numlist[0]])
+
 
 # ===== New Thing =========================================
 
@@ -41,9 +48,12 @@ def new_thing() -> Item:  # new_thing
 
     # Decide what kind of object it will be
     # - If we haven't had food for a while, let it be food
-    if random.randint(0, 10) < 3:  # TODO this is baloney
+    i = random.randint(0, 10)  # TODO This is baloney
+    if i < 2:  # TODO this is baloney
         # TODO: putting down food resets 'no_food' count
         return new_food()
-    return new_armor()
+    if i > 7:
+        return new_weapon()
+    return new_weapon()
 
 # EOF
